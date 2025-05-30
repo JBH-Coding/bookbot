8f921a678bd215d72d3b294eba2a2323b37aa09f
Justin Hooper <jhooperonline@gmail.com>
2025-05-29 23:42:07 -0600

    feat:  Add command line parsing and character/word counting.

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from stats import count_words
+from stats import count_words, order_characters
+import sys
 
 def get_book_text(filepath: str) -> str:
   with open(filepath) as file:
@@ -7,8 +8,22 @@ def get_book_text(filepath: str) -> str:
 
 
 def main():
-  filepath = "./books/frankenstein.txt"
-  book_text = get_book_text(filepath)
-  print(f"{count_words(book_text)} words found in the document")
+    if len(sys.argv) != 2:
+       print("Usage: python3 main.py <path_to_book>")
+       sys.exit(1)
+    filepath = sys.argv[1]
+    book_text = get_book_text(filepath)
+
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {filepath}...")
+    print("----------- Word Count ----------")
+    print(f"Found {count_words(book_text)} total words")
+    print("--------- Character Count -------")
+    characters = order_characters(book_text)
+    char_counts = [f"{char_count['char']}: {char_count['num']}" 
+                   for char_count in characters 
+                   if char_count['char'].isalpha()]
+    print('\n'.join(char_counts))
+    print("============= END ===============")
 
 main()
--- stats.py
+++ stats.py
@@ -2,3 +2,20 @@ def count_words(text: str) -> int:
     words = text.split()
     return len(words)
 
+def count_characters(text: str) -> dict[str,int]:
+    count = dict()
+    for char in text:
+      if char.lower() not in count:
+         count[char.lower()] = 1
+      else:
+         count[char.lower()] += 1
+    return count
+
+def sort_dict(datum: dict) -> int:
+   return datum["num"]
+
+def order_characters(text: str)->list[dict]:
+   char_dict = count_characters(text)
+   dict_list = [{"char" : key, "num" : count} for key, count in char_dict.items()]
+   dict_list.sort(reverse=True, key=sort_dict)
+   return dict_list
